18f64a8ccfd5438e47e7dbe5123db1b69b66fa7c
Mocson <kimura.ryota.as@gmail.com>
2019-02-15 01:02:41 +0900

    filePath変更

--- fileCollect.py
+++ fileCollect.py
@@ -1,5 +1,6 @@
 import maya.cmds as mc
 import subprocess
+import os
 
 def fileCollect():
     curProj = mc.workspace(q=True,fullName=True)
@@ -10,8 +11,27 @@ def fileCollect():
     l = len(a)
     for i in range(l):
         b = mc.getAttr("{}.fileTextureName".format(a[i]))
-        b = b.replace("/","\\")
-        cmd = r'copy {} {}'.format(b,siPath)
+        b2 = b.replace("/","\\")
+        cmd = r'copy {} {}'.format(b2,siPath)
         returncode = subprocess.Popen(cmd, shell=True)
+        bn = os.path.basename(b)
+        bn = "sourceimages\\" + bn
+        print bn
+        mc.setAttr("{}.fileTextureName".format(a[i]), bn, type="string" )
 
-fileCollect()
+        # c = mc.setAttr("{}.fileTextureName".format(a[i]), path, type="string" )
+
+    # for i in range(l):
+    #     c = mc.setAttr("{}.fileTextureName".format(a[i]), path, type="string" )
+# setAttr -type "string" file1.fileTextureName "D:/TESTTEST/outProjecttex/test2.tif";
+def mainWin():
+    cmds.window(title='fileCollect', w=200)
+    cmds.columnLayout(adj=True)
+    cmds.separator( h=3 )
+    cmds.text("fileCollect_V1")
+    cmds.separator( h=3 )
+    cmds.button( label = 'Do it!', w=100, h=40, command='fileCollect()')
+
+    cmds.showWindow()
+
+mainWin()
